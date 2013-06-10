db3dce5bb4eee369347fe596ac8fbf90f46eb08e
Brian Riddle <brian.riddle@tv4.se>
2013-06-10 13:50:50 +0200

    Yamling around.

--- init.el
+++ init.el
@@ -10,7 +10,12 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-js starter-kit-ruby starter-kit-bindings color-theme-solarized)
+(defvar my-packages '(color-theme-solarized
+                      starter-kit 
+                      starter-kit-js 
+                      starter-kit-ruby 
+                      starter-kit-bindings 
+                      yaml-mode)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
@@ -18,4 +23,13 @@
     (package-install p)))
 
 
+;; configure solarized
+
 (load-theme 'solarized-dark t)
+
+
+
+;;configure yaml-mode
+
+(require 'yaml-mode)
+    (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
