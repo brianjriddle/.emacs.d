abf42b056d99710b46f4536ed3a48b1e1c26be28
Brian Riddle <brian.riddle@tv4.se>
2013-06-11 15:09:19 +0200

    Don't make me cut you.

--- init.el
+++ init.el
@@ -10,13 +10,6 @@
 (set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
-;;;
-;; migrated options from my vimrc
-
-;Don't wrap text (makes windows ugly)
-;set nowrap
-(setq-default truncate-lines t)
-
 ;; taken from http://www.masteringemacs.org/articles/2011/01/19/script-files-executable-automatically/
 
 (add-hook 'after-save-hook
@@ -56,6 +49,30 @@
 (require 'yaml-mode)
     (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
+
+;;;; fix copy & paste when running in terminal on osx
+;; from
+;; http://allkindsofrandomstuff.blogspot.se/2009/09/sharing-mac-clipboard-with-emacs.html
+
+
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil)) 
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+
+;;;
+;; migrated options from my vimrc
+
+;Don't wrap text (makes windows ugly)
+;set nowrap
+(setq-default truncate-lines t)
 
 
 ;;;Need to be migrated from vimrc
